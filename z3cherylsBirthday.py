18a53543a81e39d10dbf04abe7c4fdf25e24e9ec
SF Rajaona <sfrajaona@gmail.com>
2022-07-03 23:43:04 +0100

    z3 and cvc5 py codes

---
+++ z3cherylsBirthday.py
@@ -0,0 +1,27 @@
+from z3 import * 
+import time
+
+if __name__ == '__main__':
+    try:
+        f = open("cherylsBirthday.py",'r')
+    except IOError:
+        print("I could not find the file. Please generate it with ExampleCherylsBirthday.hs")
+
+    uivar_bound = eval(f.readline())
+    xivar_bound = eval(f.readline())
+
+    month = Int('m') 
+    day = Int('n') 
+    UI = IntVector('u', uivar_bound+1)
+    XI = IntVector('x', xivar_bound+1)
+
+    phi = eval(f.readline())
+    alpha = eval(f.readline())
+
+    start_time = time.time()
+    s = Solver()
+    s.add(phi)
+    s.add(alpha)
+    print(s.check())
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print(solve(And(alpha,phi)))
